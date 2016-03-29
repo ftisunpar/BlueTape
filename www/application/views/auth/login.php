e3dea202705fd63f87e79a509f9ee9252bca5ea0
Pascal Alfadian Nugroho <pascalalfadian@live.com>
2016-03-29 12:30:57 +0700

    Simplify CSS style for login page.

--- www/application/views/auth/login.php
+++ www/application/views/auth/login.php
@@ -13,15 +13,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <body>
         <?php $this->load->view('templates/flashmessage'); ?>
         <div class="row">
-            <div class="large-3 columns">&nbsp;</div>
-            <div class="large-6 columns centered">
-                <img src="/public/images/logo.png" alt="Logo BlueTape"/>
-                <p>Silahkan login di aplikasi BlueTape, dengan menekan tombol di bawah ini. Gunakan akun
-                    <b>@unpar.ac.id</b> atau <b>@student.unpar.ac.id</b>.</p>
-                <a href="<?= $authURL; ?>" class="button">Login dengan Google</a><br/><br/>
-                <a href="https://github.com/ftisunpar/BlueTape/wiki/UserGuide" target="_blank">Petunjuk Penggunaan</a>
+            <div class="large-6 large-centered columns centered">
+                <p class="text-center">
+                    <img src="/public/images/logo.png" alt="Logo BlueTape"/><br/><br/>
+                    Silahkan login di aplikasi BlueTape, dengan menekan tombol di bawah ini. Gunakan akun
+                    <b>@unpar.ac.id</b> atau <b>@student.unpar.ac.id</b>.<br/><br/>
+                    <a href="<?= $authURL; ?>" class="button expand">Login dengan Google</a><br/><br/>
+                    <a class="text-center" href="https://github.com/ftisunpar/BlueTape/wiki/UserGuide" target="_blank">Petunjuk Penggunaan</a>
+                </p>
             </div>
-            <div class="large-3 columns">&nbsp;</div>
         </div>
 
         <script src="/public/foundation-6/js/vendor/jquery.min.js"></script>
