4a1a3e4f6c4eccaf34016fa86088a330a0317fff
Adrian Reynaldi <adrianreynaldi@yahoo.com>
2017-03-12 20:24:02 +0700

    Menambahkan tampilan untuk memasukan jadwal

--- www/application/views/EntriJadwalDosen/main.php
+++ www/application/views/EntriJadwalDosen/main.php
@@ -10,15 +10,34 @@
 		
         <div class="row">
             <div class="large-12 column">
-				
+				<div class="callout">
+					<form method="POST" action="/EntriJadwalDosen/add">
+					 <input type="hidden" name="<?= $this->security->get_csrf_token_name() ?>" value="<?= $this->security->get_csrf_hash() ?>" />
+						Hari 
+						<select name="hari"> 
+							<option> Senin </option>
+							<option> Selasa </option>
+							<option> Rabu </option>
+							<option> Kamis</option>
+							<option> Jumat </option>
+						</select><br>
+						Jam Mulai <input type="time" name="jam_mulai"> </input>
+						Durasi <input type="time" name="durasi"> </input>
+						Jenis  
+						<select name="jenis_jadwal"> 
+							<option> Konsultasi </option>
+							<option> Perwalian</option>
+							<option> Kelas </option>
+						</select>
+						Label <input type="text" name="label_jadwal"><br>
+						<input type="submit" class="button" value="Submit">
+					</form>
+				</div>
 			</div>
 		</div>
-        
-		<div class="reveal" id="asd" data-reveal>
-			test
-		</div>
-		
-	<a data-open="asd">+ Tambah Jadwal</i></a>
-	<?php $this->load->view('templates/script_foundation'); ?>
-</body>
+		<?php
+			echo $request_add_jadwal['jam_mulai'];
+		?>
+		<?php $this->load->view('templates/script_foundation'); ?>
+	</body>
 </html>
