8257485ac54e57657da66cee7f01cd1d73f0a7ea
Adrian Reynaldi <adrianreynaldi@yahoo.com>
2017-04-06 21:37:28 +0700

    menambah fungsi untuk mengambil data seluruh jadwal dosen dari database

--- www/application/models/JadwalDosen_model.php
+++ www/application/models/JadwalDosen_model.php
@@ -4,7 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class JadwalDosen_model extends CI_Model {
 
-    
+    public $user;
+	public $hari;
+	public $jam_mulai;
+	public $durasi;
+	public $jenis;
+	public $label;
+	
     /**
      * Mendapatkan seluruh request dari email tertentu
      * @param type $email email yang melakukan request atau NULL untuk semua
@@ -31,7 +37,24 @@ class JadwalDosen_model extends CI_Model {
      * diperbolehkan.
      */
     public function addJadwal($data) {
+		$this->user = $data['user'];
+		$this->hari = $data['hari'];
+		$this->jam_mulai = $data['jam_mulai'];
+		$this->durasi = $data['durasi'];
+		$this->jenis = $data['jenis_jadwal'];
+		$this->label = $data['label_jadwal'];
 		
+		$this->db->insert('jadwal', $this);
     }
-
+	
+	public function getAllJadwal(){
+		$query = $this->db->get('jadwal', 10);
+                return $query->result();
+	}
+	
+	public function getJadwalByUsername($user){
+		$query = $this->db->get_where('jadwal',array('user'=>$user));
+                return $query->result();
+	}
+	
 }
