127e0e162c86bf08f36b7007bb1f81d6abbfdf8a
Adrian Reynaldi <adrianreynaldi@yahoo.com>
2017-04-16 22:00:42 +0700

    Menambahkan fungsi untuk ekspor ke XLS

--- www/application/controllers/LihatJadwalDosen.php
+++ www/application/controllers/LihatJadwalDosen.php
@@ -14,6 +14,7 @@
 			}
 			$this->load->library('bluetape');
 			$this->load->library('excel');
+			$this->load->library('session');
 			$this->load->model('JadwalDosen_model');
 			$this->load->database();
 		}
@@ -22,87 +23,126 @@
 			
 			// Retrieve logged in user data
 			$userInfo = $this->Auth_model->getUserInfo();
+			
 			$dataJadwal = $this->JadwalDosen_model->getAllJadwal();
+			$dataJadwalPerUser=array();
+			foreach($dataJadwal as $key => $indexValue){
+				$dataJadwalPerUser[$indexValue->user][$key] = $indexValue;  // dimensi pertama indexnya adalah user
+			}
+			ksort($dataJadwalPerUser);
+			$_SESSION['dataJadwalPerUser']=$dataJadwalPerUser; //agar dapat dibaca oleh controller LihatJadwalDosen fungsi export() 
+			
 			$this->load->view('LihatJadwalDosen/main', array(
             'currentModule' => get_class(),
-			'dataJadwal'    =>$dataJadwal
+			'dataJadwalPerUser'    =>$dataJadwalPerUser
 			));
 		}
 		
 		public function export(){
-			$request_print = $this->session->userdata('request_add_jadwal');
+			$dataToExport = $_SESSION["dataJadwalPerUser"];
 			
-			// ------------------------------------------------------------ TEMPLATE JADWAL DOSEN -----------------------------------------------------------------------------
-			$this->excel->setActiveSheetIndex(0);
-			$this->excel->getActiveSheet()->setTitle('Jadwal Dosen');
-			//Menulis header file XLS
-			$this->excel->getActiveSheet()->setCellValue('A1', 'JADWAL AKTIVITAS DOSEN');
-			$this->excel->getActiveSheet()->getStyle("A1")->getFont()->setBold(true);
-			$this->excel->getActiveSheet()->setCellValue('A2', 'Dosen :');
-			$this->excel->getActiveSheet()->getStyle("A2")->getFont()->setBold(true);
+			// ------------------------------------------------------------ TEMPLATE TABEL JADWAL DOSEN -----------------------------------------------------------------------------
+			$startHourRow=5;
+			$endHourRow=$startHourRow+10;	
+			$startColoredCell=-2; // minus dua karena perbedaan penomoran baris di excel dengan jam paling pagi bisa dicatatnya sebuah jadwal
+			$titleRow=1;
+			$nameRow=2;
+			$dayRow=4;
+			$keteranganRow=17;
+			$nextTableRowsAdder=19;
 			
-			//Menulis hari-hari dalam tabel jadwal dosen
-			$this->excel->getActiveSheet()->setCellValue('B4', 'Senin');
-			$this->excel->getActiveSheet()->setCellValue('C4', 'Selasa');
-			$this->excel->getActiveSheet()->setCellValue('D4', 'Rabu');
-			$this->excel->getActiveSheet()->setCellValue('E4', 'Kamis');
-			$this->excel->getActiveSheet()->setCellValue('F4', 'Jumat');
-			//Menentukan style dari border
-			$borderStyleArray = array(
-			'borders' => array(
-			'allborders' => array(
-			'style' => PHPExcel_Style_Border::BORDER_THIN
-			)
-			)
-			);
-			
-			//Menulis bagian keterangan
-			$this->excel->getActiveSheet()->setCellValue('A17', 'Keterangan :');
-			$this->excel->getActiveSheet()->getStyle('B17')->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID)->getStartColor()->setRGB('FEFF00');
-			$this->excel->getActiveSheet()->getStyle('B18') ->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID)->getStartColor()->setRGB('92D14F');
-			$this->excel->getActiveSheet()->setCellValue('C17', 'Waktu Konsultasi');
-			$this->excel->getActiveSheet()->setCellValue('C18', 'Jika Dijadwalkan');
-			
-			//Menentukan letak border
-			$this->excel->getActiveSheet()->getStyle('A4:F14')->applyFromArray($borderStyleArray);
-			$this->excel->getActiveSheet()->getStyle('B17')->applyFromArray($borderStyleArray);
-			$this->excel->getActiveSheet()->getStyle('B18')->applyFromArray($borderStyleArray);
-			unset($borderStyleArray);
-			
-			//Membuat semua tulisan dalam tabel menggunakan align center
-			$this->excel->getActiveSheet()->getStyle('A4:F14')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
-			for($col = ord('A'); $col <= ord('F'); $col++){
-				//mengatur lebar setiap kolom $col
-				$this->excel->getActiveSheet()->getColumnDimension(chr($col))->setWidth(15);
-			}
-			for($row = 4; $row <= 14; $row++){
-				//mengatur tinggi setiap baris $row
-				$this->excel->getActiveSheet()->getRowDimension($row)->setRowHeight(20);
-			}
-			//-----------------------------------------------------------------------------------------------------------------------------------------------------------
-			
-			//menulis jam-jam dalam tabel jadwal dosen
-			$jam=7;
-			$column_hari=$this->hari_ke_kolom($request_print['hari']);
-			if($request_print['jenis_jadwal']=="konsultasi"){
-				$color="FEFF00";
-			}
-			else{
-				$color="92D14F";
-			}
-			for($i=5 ; $i<15 ; $i++){
-				$column='A'.$i;
-				$this->excel->getActiveSheet()->setCellValue($column, ($jam).'-'.($jam+1));
+			$idx=0;
+			$sheetIdx=0;
+			foreach($dataToExport as $currRow){	
+				$objWorkSheet = $this->excel->createSheet($sheetIdx);
+				$this->excel->setActiveSheetIndex(0);
+				$this->excel->getActiveSheet()->setTitle('Jadwal '.$currRow[$idx]->name);
 				
-				$temp=idate('H',strtotime($request_print['jam_mulai']))+idate('H',strtotime($request_print['durasi']));
-				if($jam>=idate('H', strtotime($request_print['jam_mulai'])) && $jam<$temp){
-					$column_hari_used=$column_hari.$i;
-					$this->excel->getActiveSheet()->getStyle($column_hari_used)->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID)->getStartColor()->setRGB($color);
+				//Menulis header tabel
+				$titleCell='A'.$titleRow;
+				$this->excel->getActiveSheet()->setCellValue($titleCell, 'JADWAL AKTIVITAS DOSEN');
+				$this->excel->getActiveSheet()->getStyle($titleCell)->getFont()->setBold(true);
+				$this->excel->getActiveSheet()->setCellValue('A'.$nameRow, 'Dosen :'.$currRow[$idx]->name); // menulis nama dosen yang bersangkutan 
+				
+				$this->excel->getActiveSheet()->getStyle('A'.$nameRow)->getFont()->setBold(true);
+				
+				//Menulis hari-hari dalam tabel jadwal dosen
+				for($dayCol='B';$dayCol<='F';$dayCol++){
+					$dayCell=$dayCol.$dayRow;
+					$hari=$this->kolom_ke_hari($dayCol);
+					$this->excel->getActiveSheet()->setCellValue($dayCell, $hari);
 				}
 				
-				$jam++;
+				//Menentukan style dari border
+				$borderStyleArray = array(
+				'borders' => array(
+				'allborders' => array(
+				'style' => PHPExcel_Style_Border::BORDER_THIN
+				)
+				)
+				);
+				
+				//Menulis bagian keterangan
+				$this->excel->getActiveSheet()->setCellValue('A'.$keteranganRow, 'Keterangan :');
+				$this->excel->getActiveSheet()->getStyle('B'.$keteranganRow)->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID)->getStartColor()->setRGB('FEFF00');
+				$this->excel->getActiveSheet()->getStyle('B'.($keteranganRow+1)) ->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID)->getStartColor()->setRGB('92D14F');
+				$this->excel->getActiveSheet()->setCellValue('C'.$keteranganRow, 'Waktu Konsultasi');
+				$this->excel->getActiveSheet()->setCellValue('C'.($keteranganRow+1), 'Jika Dijadwalkan');
+				
+				//membuat border pada tabel
+				$this->excel->getActiveSheet()->getStyle('A'.$dayRow.':F'.($dayRow+10))->applyFromArray($borderStyleArray);
+				$this->excel->getActiveSheet()->getStyle('B'.$keteranganRow)->applyFromArray($borderStyleArray);
+				$this->excel->getActiveSheet()->getStyle('B'.($keteranganRow+1))->applyFromArray($borderStyleArray);
+				unset($borderStyleArray);
+				
+				//Membuat semua tulisan dalam tabel menggunakan align center
+				$this->excel->getActiveSheet()->getStyle('A'.$dayRow.':F'.($dayRow+10))->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+				for($col = ord('A'); $col <= ord('F'); $col++){
+					//mengatur lebar setiap kolom $col
+					$this->excel->getActiveSheet()->getColumnDimension(chr($col))->setWidth(15);
+				}
+				for($row = $dayRow; $row <= ($dayRow+10); $row++){
+					//mengatur tinggi setiap baris $row
+					$this->excel->getActiveSheet()->getRowDimension($row)->setRowHeight(20);
+				}
+				//--------------------------------------------------------------------------------END TEMPLATE---------------------------------------------------------------------------
+				
+				//menulis jam-jam dalam tabel jadwal dosen
+				$jam=7;
+				for($i=$startHourRow ; $i<($endHourRow) ; $i++){
+					$column='A'.$i;
+					$this->excel->getActiveSheet()->setCellValue($column, ($jam).'-'.($jam+1));
+					$jam++;
+				}
+				foreach($currRow as $dataHariIni){
+					if($dataHariIni!=null){
+						$colHari=$this->hari_ke_kolom($dataHariIni->hari);
+						$row_jam_mulai= $dataHariIni->jam_mulai + $startColoredCell;
+						$row_jam_selesai = $row_jam_mulai + $dataHariIni->durasi;
+						if($dataHariIni->jenis=="konsultasi"){
+							$color="FEFF00";
+						}
+						else{
+							$color="92D14F"; //#c1c1c1
+						}
+						
+						$counterForLabel=0;
+						for($rowHour=$row_jam_mulai ; $rowHour<$row_jam_selesai; $rowHour++){
+							$cellToBeColored=$colHari.$rowHour;
+							$this->excel->getActiveSheet()->getStyle($cellToBeColored)->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID)->getStartColor()->setRGB($color);
+							if( ($counterForLabel) == (int)($dataHariIni->durasi/2) || ($dataHariIni->durasi)==1){ 
+								$this->excel->getActiveSheet()->setCellValue($cellToBeColored, $dataHariIni->label);
+							}
+							$counterForLabel++;
+						}
+						
+					}
+				}
+				
+				$idx++;
+				$sheetIdx;
+				
 			}
-			
 			$filename='Jadwal Dosen.xlsx'; //Nama file XLS yang akan dibuat
 			header('Content-type: application/vnd.ms-excel');
 			header('Content-Disposition: attachment;filename="'. $filename.'"');
@@ -122,12 +162,27 @@
 				return "B";
 				case "Selasa":
 				return "C";
-				case "Rabu":
-				return "D";
-				case "Kamis":
-				return "E";
-				case "Jumat":
-				return "F";
+			case "Rabu":
+			return "D";
+			case "Kamis":
+			return "E";
+			case "Jumat":
+			return "F";
+			}
+		}
+		
+		public function kolom_ke_hari($col){
+			switch ($col) {
+				case "B":
+				return "Senin";
+				case "C":
+				return "Senin";
+				case "D":
+				return "Rabu";
+				case "E":
+				return "Kamis";
+				case "F":
+				return "Jumat";
 			}
 		}
 	}
