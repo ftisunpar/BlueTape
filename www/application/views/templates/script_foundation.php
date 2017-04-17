843a9a58d2e9772c873a4553ab4fe8823651749b
Adrian Reynaldi <adrianreynaldi@yahoo.com>
2017-04-17 15:11:11 +0700

    mengembalikan ke awal

--- www/application/views/templates/script_foundation.php
+++ www/application/views/templates/script_foundation.php
@@ -3,6 +3,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><script src="/public/js/vendor/jquery.min.js"></script>
 <script src="/public/js/vendor/what-input.min.js"></script>
 <script src="/public/js/foundation.min.js"></script>
-<script src="/public/js/foundation.js"></script>
 <script src="/public/js/foundation-datepicker.min.js"></script>
 <script src="/public/js/app.js"></script>
