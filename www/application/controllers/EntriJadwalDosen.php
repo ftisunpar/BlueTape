76a20c9eff2929536adc041980a43eb7fe955031
Adrian Reynaldi <adrianreynaldi@yahoo.com>
2017-05-02 04:15:51 +0700

    Merapikan kode dengan auto-indent Netbeans

--- www/application/controllers/EntriJadwalDosen.php
+++ www/application/controllers/EntriJadwalDosen.php
@@ -14,52 +14,53 @@ class EntriJadwalDosen extends CI_Controller {
         }
         $this->load->library('bluetape');
         $this->load->model('JadwalDosen_model');
-		$this->load->model('Auth_model');
+        $this->load->model('Auth_model');
         $this->load->database();
     }
 
     public function index() {
-         // Retrieve logged in user data
+        // Retrieve logged in user data
         $userInfo = $this->Auth_model->getUserInfo();
-		$dataJadwal = $this->JadwalDosen_model->getJadwalByUsername($userInfo['email']);
-		$namaHari = $this->JadwalDosen_model->getNamaHari();
+        $dataJadwal = $this->JadwalDosen_model->getJadwalByUsername($userInfo['email']);
+        $namaHari = $this->JadwalDosen_model->getNamaHari();
         $this->load->view('EntriJadwalDosen/main', array(
             'currentModule' => get_class(),
-			'request_add_jadwal' => $this->session->userdata('request_add_jadwal'),
-			'dataJadwal'	=> $dataJadwal,
-			'namaHari'		=> $namaHari
+            'request_add_jadwal' => $this->session->userdata('request_add_jadwal'),
+            'dataJadwal' => $dataJadwal,
+            'namaHari' => $namaHari
         ));
     }
-	
-	public function add() {
-		$userInfo = $this->Auth_model->getUserInfo();
-		$data = array(
-			'user'	=> $userInfo['email'],
-			'hari' => $this->input->post('hari'),
-			'jam_mulai' => $this->input->post('jam_mulai'),
-			'durasi' => $this->input->post('durasi'),
-			'jenis_jadwal' => $this->input->post('jenis_jadwal'),
-			'label_jadwal' => $this->input->post('label_jadwal')
-		);
-		//$this->session->set_userdata('request_add_jadwal',$data);
-		$this->JadwalDosen_model->addJadwal($data);
-		 header('Location: /EntriJadwalDosen');
-	}
-	
-	public function update($id_jadwal) {
-		$userInfo = $this->Auth_model->getUserInfo();
-		$data = array(
-			'hari' => $this->input->post('hari'),
-			'jam_mulai' => $this->input->post('jam_mulai'),
-			'durasi' => $this->input->post('durasi'),
-			'jenis' => $this->input->post('jenis_jadwal'),
-			'label' => $this->input->post('label_jadwal')
-		);
-		$this->JadwalDosen_model->updateJadwal($id_jadwal, $data);
-		 header('Location: /EntriJadwalDosen');
-	}
-	
-	public function getDataJadwal($id_jadwal){
-		echo $this->JadwalDosen_model->getJadwalByIdJadwal($id_jadwal);
-	}
+
+    public function add() {
+        $userInfo = $this->Auth_model->getUserInfo();
+        $data = array(
+            'user' => $userInfo['email'],
+            'hari' => $this->input->post('hari'),
+            'jam_mulai' => $this->input->post('jam_mulai'),
+            'durasi' => $this->input->post('durasi'),
+            'jenis_jadwal' => $this->input->post('jenis_jadwal'),
+            'label_jadwal' => $this->input->post('label_jadwal')
+        );
+        //$this->session->set_userdata('request_add_jadwal',$data);
+        $this->JadwalDosen_model->addJadwal($data);
+        header('Location: /EntriJadwalDosen');
+    }
+
+    public function update($id_jadwal) {
+        $userInfo = $this->Auth_model->getUserInfo();
+        $data = array(
+            'hari' => $this->input->post('hari'),
+            'jam_mulai' => $this->input->post('jam_mulai'),
+            'durasi' => $this->input->post('durasi'),
+            'jenis' => $this->input->post('jenis_jadwal'),
+            'label' => $this->input->post('label_jadwal')
+        );
+        $this->JadwalDosen_model->updateJadwal($id_jadwal, $data);
+        header('Location: /EntriJadwalDosen');
+    }
+
+    public function getDataJadwal($id_jadwal) {
+        echo $this->JadwalDosen_model->getJadwalByIdJadwal($id_jadwal);
+    }
+
 }
