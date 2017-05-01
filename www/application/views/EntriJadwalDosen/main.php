84e2bcc6531dfe432fe21b363432ae9b40885c15
Adrian Reynaldi <adrianreynaldi@yahoo.com>
2017-05-02 03:29:26 +0700

    Mengatur kode agar menerima value dari hari berupa integer(sebelumnya valuenya berupa string)

--- www/application/views/EntriJadwalDosen/main.php
+++ www/application/views/EntriJadwalDosen/main.php
@@ -17,37 +17,29 @@
 						<input type="hidden" name="<?= $this->security->get_csrf_token_name() ?>" value="<?= $this->security->get_csrf_hash() ?>" />
 						Hari 
 						<select name="hari"> 
-							<option> Senin </option>
-							<option> Selasa </option>
-							<option> Rabu </option>
-							<option> Kamis</option>
-							<option> Jumat </option>
+							<?php
+								$hariValue=0;
+								foreach($namaHari as $hari){ ?>
+								<option value="<?=$hariValue?>"> <?=$hari?> </option>
+								
+								<?php 
+									$hariValue++;
+								}
+							?>
 						</select><br>
 						Jam Mulai
 						<select name="jam_mulai"> 
-							<option value="7"> 07:00 </option>
-							<option value="8"> 08:00 </option>
-							<option value="9"> 09:00 </option>
-							<option value="10"> 10:00 </option>
-							<option value="11"> 11:00 </option>
-							<option value="12"> 12:00 </option>
-							<option value="13"> 13:00 </option>
-							<option value="14"> 14:00 </option>
-							<option value="15"> 15:00 </option>
+							<?php for($i=7; $i<=16 ; $i++){ ?>
+								<option value="<?php echo $i ?>"> <?php echo $i ?>:00 </option>
+							<?php } ?>
 						</select><br>
 					</div>
 					<div class=" large-4 columns">
 						Durasi
 						<select name="durasi"> 
-							<option value="1"> 1 jam </option>
-							<option value="2"> 2 jam </option>
-							<option value="3"> 3 jam </option>
-							<option value="4"> 4 jam</option>
-							<option value="5"> 5 jam </option>
-							<option value="6"> 6 jam </option>
-							<option value="7"> 7 jam </option>
-							<option value="8"> 8 jam </option>
-							<option value="9"> 9 jam </option>
+							<?php for($i=1; $i<=9 ; $i++){ ?>
+								<option value="<?php echo $i ?>"> <?php echo $i ?> jam </option>
+							<?php } ?>
 						</select><br>
 						Jenis  
 						<select name="jenis_jadwal"> 
@@ -57,155 +49,145 @@
 						</select>
 					</div>
 					<div class="large-4 columns">
-						
 						Label <input type="text" name="label_jadwal"><br>
 						<input type="submit" class="button" value="Submit">
 					</form>
 				</div>
 			</div>
-			<!-- ================================================================================================================================================== -->
+			<!-- ===================================================================== Pembentukan Tabel ============================================================================= -->
 			
 			
 			email : <?php echo $dataJadwal[0]->user;?>
 			<div class="table-scroll" id="jadwal_table">
-				<table>
-					<?php
-						$nama_hari=['Senin','Selasa','Rabu','Kamis','Jumat'];
-						?><tr> <td style='width:10%'></td>
+				<table border=1>
+					<tr> 
+						<td style='width:10%'></td>
 						<?php
 							for($i=0;$i<5;$i++){
-								echo "<td style='width:18%'> $nama_hari[$i] </td>";
+								echo "<td style='width:18%'> $namaHari[$i] </td>"; //Membuat Header Tabel yang berisi daftar hari
 							}
 						?>
 					</tr>
 					<?php
 						//GENERATE BODY UTAMA TABEL
-						$cellRowID=0;
+						$cellRowID=1;
 						for($i=7; $i<17 ;$i++){
 							echo "<tr><td>".$i."-".($i+1);
-							$cellColID=0;
+							$cellColID=1;
 							for($j=0;$j<5;$j++){
-								echo"<td align='center' id='cell".$cellColID."-".$cellRowID."'>"."</td>";
+								echo"<td align='center' id='cell".$cellRowID."-".$cellColID."'>"."</td>";
 								$cellColID++;
 							}
 							$cellRowID++;
 						} 
-						
 						// MEWARNAI TABEL
 						$rowIdx=1;
+						$spanCounter=[0,0,0,0,0,0,0,0,0,0];
 						$cellRowID=0;
-						for($i=7; $i<17 ;$i++){
-							
-							foreach($dataJadwal as $dataHariIni){
-								$colIdx=1;
-								$cellColID=0;
-								for($j=0;$j<5;$j++){ 
-									if($dataHariIni!=null){
-										if($dataHariIni->hari==$nama_hari[$j]){
-											$jam_selesai = $dataHariIni->jam_mulai + $dataHariIni->durasi;
-											if($dataHariIni->jenis=="konsultasi"){
-												$color="#FEFF00";
-											}
-											else if($dataHariIni->jenis == "kelas"){
-												$color="#FFFFFF";
-											}
-											else{
-												$color="#92D14F"; 
-											}
-											if($i >= $dataHariIni->jam_mulai && $i < $jam_selesai){
-											?>
-											<script type="text/javascript">
-												var table = document.getElementById('jadwal_table');
-												var rows = table.getElementsByTagName('tr')
-												rows[<?php echo $rowIdx; ?>].cells[<?php echo ($colIdx); ?>].style.backgroundColor = "<?php echo $color; ?>";
-											</script>
-											<?php
-												if( ($i - $dataHariIni->jam_mulai) ==(int)(($dataHariIni->durasi)/2) || ($dataHariIni->durasi)==1){ //menampilkan label ditengah-tengah
-												?>
-												<script>
-													rows[<?php echo $rowIdx; ?>].cells[<?php echo ($colIdx); ?>].innerHTML= "<?php echo $dataHariIni->label ?>";
-												</script>
-												<?php
-												}
-											?>
-											<script>
-												var $cellLocation = "#cell<?php echo $cellColID;?>-<?php echo $cellRowID;?>";								// nama dengan pagar
-												var $cellLocationChangeName = "cell<?php echo $cellColID;?>-<?php echo $cellRowID;?>";						// nama tanpa pagar
-												var $cellName = document.getElementById($cellLocationChangeName).name = "<?php echo $dataHariIni->id ?>";	
-												$(document).on("click", $cellLocation, function(){
-													$('#edit').foundation('open');	
-													
-													var $id_jadwal = rows[<?php echo $rowIdx; ?>].cells[<?php echo ($colIdx); ?>].name;
-													$("#aid_jadwal").html( $id_jadwal );
-													$('input[name="id_jadwal_parameter"]').val($id_jadwal);
-												});
-											</script>
-											<?php
-											}
-										}
-									}
-									$cellColID++;
-									$colIdx++;
-								}
+						foreach($dataJadwal as $dataHariIni){
+							$colIdx=$dataHariIni->hari + 1; 		// + 1 karena perbedaan selisih index tabel dan value hari di database 
+							$rowIdx=$dataHariIni->jam_mulai - 6; 	// + 1 karena perbedaan selisih index tabel dan value jam_mulai di database 
+							if($dataHariIni->jenis=="konsultasi"){
+								$color="#FEFF00";
 							}
-							$cellRowID++;
-							$rowIdx++;
-						} 
+							else if($dataHariIni->jenis == "kelas"){
+								$color="#FFFFFF";
+							}
+							else{
+								$color="#92D14F"; 
+							}
+						?>
+						<script type="text/javascript">
+							var table = document.getElementById('jadwal_table');
+							var rows = table.getElementsByTagName('tr');
+							var $cellLocation = "#cell<?php echo $rowIdx;?>-<?php echo $colIdx;?>";	
+							
+							$($cellLocation).css('background-color', '<?php echo $color;?>');
+							$($cellLocation).attr('rowspan', <?php echo $dataHariIni->durasi?> );
+							
+							//menghapus cell-cell yang tergeser karena rowspan
+							for (i = <?php echo ($rowIdx+1); ?> ; i < <?php echo ($rowIdx + $dataHariIni->durasi); ?>; i++) { 
+								$("#cell"+i+"-"+<?php echo $colIdx; ?>).remove();
+							}
+							$($cellLocation).html("<?php echo $dataHariIni->label ?>");
+							
+							//membuat cell-cell yang telah diwarnai jadi memunculkan pop-up untuk mengedit jadwal ketika diklik oleh mouse
+							$(document).on("click", $cellLocation, function(){
+								var $menuName = "#edit_menu<?php echo $dataHariIni->id ?>";
+								$($menuName).foundation('open');	
+							});
+						</script>
+						<?php
+						}
+						$cellRowID++;
+						$rowIdx++;	
 					?>
 				</table>
 			</div>
 			
 			
-			<div id="edit" class="large-12 column reveal"  data-reveal style="display: block; right:30%;" >
-				<form method="POST" action="/EntriJadwalDosen/update">
-					<input type="hidden" name="<?= $this->security->get_csrf_token_name() ?>" value="<?= $this->security->get_csrf_hash() ?>" />
-					<input type="hidden" name="id_jadwal_parameter"> </a> <br>
-					<!--  <a id="aid_jadwal"> </a> <br>  //Sekedar Keperluan Testing saja -->
+			<?php $this->load->view('templates/script_foundation'); ?>
+			<!-- ===================================================================== END Pembentukan Tabel ============================================================================= -->
+			<?php 
+				
+				foreach($dataJadwal as $dataHariIni) { ?>
+				<div id="edit_menu<?php echo $dataHariIni->id ?>" class="reveal"  data-reveal >
+					<form name="form<?php echo $dataHariIni->id ?>" method="POST" action="/EntriJadwalDosen/update/<?php echo $dataHariIni->id ?>">
+						<input type="hidden" name="<?= $this->security->get_csrf_token_name() ?>" value="<?= $this->security->get_csrf_hash() ?>" />
+					<input type="hidden" name="id_jadwal_parameter" value="<?php echo $dataHariIni->id ?>"> </a> <br>
 					Hari 
 					<select name="hari"> 
-						<option> Senin </option>
-						<option> Selasa </option>
-						<option> Rabu </option>
-						<option> Kamis</option>
-						<option> Jumat </option>
+						<?php
+							$hariValue=0;
+							foreach($namaHari as $hari){ 
+								if($hariValue == $dataHariIni->hari){ ?>
+								<option value="<?=$hariValue?>" selected> <?=$hari?> </option>
+								<?php }
+								else {
+								?>
+								<option value="<?=$hariValue?>"> <?=$hari?> </option>
+								<?php 
+								}
+								$hariValue++;
+							}
+						?>
 					</select><br>
 					Jam Mulai
 					<select name="jam_mulai"> 
-						<option value="7"> 07:00 </option>
-						<option value="8"> 08:00 </option>
-						<option value="9"> 09:00 </option>
-						<option value="10"> 10:00 </option>
-						<option value="11"> 11:00 </option>
-						<option value="12"> 12:00 </option>
-						<option value="13"> 13:00 </option>
-						<option value="14"> 14:00 </option>
-						<option value="15"> 15:00 </option>
-						<option value="15"> 16:00 </option>
+						<?php for($i=7; $i<=16 ; $i++){
+							if($i == $dataHariIni->jam_mulai){ ?>
+							<option value="<?php echo $i ?>" selected> <?php echo $i ?>:00 </option>
+							<?php }
+							else{
+							?>
+							<option value="<?php echo $i ?>"> <?php echo $i ?>:00 </option>
+							<?php } 
+						}?>
 					</select><br>
 					Durasi
 					<select name="durasi"> 
-						<option value="1"> 1 jam </option>
-						<option value="2"> 2 jam </option>
-						<option value="3"> 3 jam </option>
-						<option value="4"> 4 jam</option>
-						<option value="5"> 5 jam </option>
-						<option value="6"> 6 jam </option>
-						<option value="7"> 7 jam </option>
-						<option value="8"> 8 jam </option>
-						<option value="9"> 9 jam </option>
+						<?php for($i=1; $i<=9 ; $i++){ 
+							if($i == $dataHariIni->durasi){ ?>
+							<option value="<?php echo $i ?>" selected> <?php echo $i ?> jam </option>
+							<?php
+								} else {
+							?>
+							<option value="<?php echo $i ?>"> <?php echo $i ?> jam </option>
+							<?php }
+						}?>
 					</select><br>
 					Jenis  
 					<select name="jenis_jadwal"> 
-						<option value="konsultasi"> Konsultasi </option>
-						<option value="terjadwal"> Terjadwal</option>
-						<option value="kelas"> Kelas </option>
+						<option value="konsultasi" <?php if($dataHariIni->jenis == 'konsultasi') echo "selected";?> > Konsultasi </option>
+						<option value="terjadwal" <?php if($dataHariIni->jenis == 'terjadwal') echo "selected";?>> Terjadwal</option>
+						<option value="kelas" <?php if($dataHariIni->jenis == 'kelas') echo "selected";?>> Kelas </option>
 					</select>
-					Label <input type="text" name="label_jadwal"><br>
-					<input type="submit" class="button" value="Save">
-				</div>
-				
+					Label <input type="text" name="label_jadwal" value="<?php echo $dataHariIni->label;?>"><br> 
+					<input type="submit" name="submitId<?php echo $dataHariIni->id ?>" class="button" value="Save">
+				</form>
 			</div>
-			
-			<?php $this->load->view('templates/script_foundation'); ?>
-		</body>
-	</html>																												
+		<?php } ?>
+	</div>
+	<?php $this->load->view('templates/script_foundation'); ?>
+</body>
+</html>																													
