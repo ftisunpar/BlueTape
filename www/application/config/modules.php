a006587341221a49e85c080f0328f25c9ac0e1fb
Adrian Reynaldi <adrianreynaldi@yahoo.com>
2017-05-05 09:39:12 +0700

    Mengubah "dosen.it" dan "mahasiswa.it" menjadi "dosen.informatika" dan "mahasiswa.informatika"

--- www/application/config/modules.php
+++ www/application/config/modules.php
@@ -17,8 +17,8 @@ $config['modules'] = array(
     'TranskripManage' => array('root', 'tu.ftis'),
     'PerubahanKuliahRequest' => array('root', 'staf.unpar'),
     'PerubahanKuliahManage' => array('root', 'tu.ftis'),
-	'EntriJadwalDosen' => array('root', 'dosen.ftis'),
-	'LihatJadwalDosen' => array('root', 'mahasiswa.ftis', 'dosen.ftis')
+	'EntriJadwalDosen' => array('root', 'dosen.informatika'),
+	'LihatJadwalDosen' => array('root', 'mahasiswa.informatika', 'dosen.informatika')
 );
 
 $config['roles'] = array(
@@ -26,6 +26,6 @@ $config['roles'] = array(
     'tu.ftis' => array('shao.wei@unpar.ac.id', 'pranyoto@unpar.ac.id', 'walip@unpar.ac.id'),
     'mahasiswa.ftis' => '7[123]\\d{5}@student\\.unpar\\.ac\\.id',
     'staf.unpar' => '.+@unpar\\.ac\\.id',
-	'dosen.ftis' => array ('cheni@unpar.ac.id', 'mariskha@unpar.ac.id', 'nico@unpar.ac.id', 'anung@unpar.ac.id', 'moertini@unpar.ac.id', 'aditya-bagoes@unpar.ac.id', 'chandraw@unpar.ac.id', 'elisatih@unpar.ac.id', 'gkarya@unpar.ac.id', 'husnulhakim@unpar.ac.id', 'joanna@unpar.ac.id', 'lionov@unpar.ac.id', 'luciana@unpar.ac.id', 'claudio-fransicus@unpar.ac.id', 'pascal@unpar.ac.id', 'rosad5@unpar.ac.id', 'vania-natali@unpar.ac.id', 'reynaldi95@gmail.com'),
-	'mahasiswa.ftis' => '73\\d{5}@student\\.unpar\\.ac\\.id'
+	'dosen.informatika' => array ('cheni@unpar.ac.id', 'mariskha@unpar.ac.id', 'nico@unpar.ac.id', 'anung@unpar.ac.id', 'moertini@unpar.ac.id', 'aditya-bagoes@unpar.ac.id', 'chandraw@unpar.ac.id', 'elisatih@unpar.ac.id', 'gkarya@unpar.ac.id', 'husnulhakim@unpar.ac.id', 'joanna@unpar.ac.id', 'lionov@unpar.ac.id', 'luciana@unpar.ac.id', 'claudio-fransicus@unpar.ac.id', 'pascal@unpar.ac.id', 'rosad5@unpar.ac.id', 'vania-natali@unpar.ac.id', 'reynaldi95@gmail.com'),
+	'mahasiswa.informatika' => '73\\d{5}@student\\.unpar\\.ac\\.id'
 );
