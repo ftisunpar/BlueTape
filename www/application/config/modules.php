e1db9de2a5268154e5870ba073058b6411a54a5e
stephenhadi <stephenhadi123@gmail.com>
2020-11-12 16:59:49 +0700

    fix mahasiswa npm baru tidak dapat login

--- www/application/config/modules.php
+++ www/application/config/modules.php
@@ -17,15 +17,15 @@ $config['modules'] = array(
     'TranskripManage' => array('root', 'tu.ftis'),
     'PerubahanKuliahRequest' => array('root', 'staf.unpar'),
     'PerubahanKuliahManage' => array('root', 'tu.ftis'),
-	  'EntriJadwalDosen' => array('root', 'dosen.informatika' ),
-	  'LihatJadwalDosen' => array('root', 'mahasiswa.informatika', 'dosen.informatika')
+	'EntriJadwalDosen' => array('root', 'dosen.informatika' ),
+	'LihatJadwalDosen' => array('root', 'mahasiswa.informatika', 'dosen.informatika')
 );
 
 $config['roles'] = array(
     'root' => array('pascal@unpar.ac.id', 'shao.wei@unpar.ac.id','stephenhadi123@gmail.com'),
     'tu.ftis' => array('shao.wei@unpar.ac.id', 'purnomo@unpar.ac.id', 'walip@unpar.ac.id'),
-    'mahasiswa.ftis' => '(7[123]\\d{5})|(20[1-9][0-9]7[123][0-9]{4})|(61[678][0-9]{7})@student\\.unpar\\.ac\\.id',
+    'mahasiswa.ftis' => '(7[123]\\d{4})|(20[1-9][0-9]7[123][0-9]{4})|(61[678][0-9]{7})@student\\.unpar\\.ac\\.id',
     'staf.unpar' => '.+@unpar\\.ac\\.id',
-	  'dosen.informatika' => array ('cheni@unpar.ac.id', 'mariskha@unpar.ac.id', 'anung@unpar.ac.id', 'moertini@unpar.ac.id', 'natalia@unpar.ac.id', 'chandraw@unpar.ac.id', 'elisatih@unpar.ac.id', 'gkarya@unpar.ac.id', 'husnulhakim@unpar.ac.id', 'joanna@unpar.ac.id', 'lionov@unpar.ac.id', 'luciana@unpar.ac.id', 'pascal@unpar.ac.id', 'rosad5@unpar.ac.id', 'vania.natali@unpar.ac.id', 'kristopher.h@unpar.ac.id', 'raymond.chandra@unpar.ac.id', 'keenan.leman@unpar.ac.id', 'ignasiuserwin@unpar.ac.id'),
-	  'mahasiswa.informatika' => '73\\d{5}@student\\.unpar\\.ac\\.id'
+	'dosen.informatika' => array ('cheni@unpar.ac.id', 'mariskha@unpar.ac.id', 'anung@unpar.ac.id', 'moertini@unpar.ac.id', 'natalia@unpar.ac.id', 'chandraw@unpar.ac.id', 'elisatih@unpar.ac.id', 'gkarya@unpar.ac.id', 'husnulhakim@unpar.ac.id', 'joanna@unpar.ac.id', 'lionov@unpar.ac.id', 'luciana@unpar.ac.id', 'pascal@unpar.ac.id', 'rosad5@unpar.ac.id', 'vania.natali@unpar.ac.id', 'kristopher.h@unpar.ac.id', 'raymond.chandra@unpar.ac.id', 'keenan.leman@unpar.ac.id', 'ignasiuserwin@unpar.ac.id'),
+	'mahasiswa.informatika' => '(73\\d{4}|618\\d{7})@student\\.unpar\\.ac\\.id'
 );
