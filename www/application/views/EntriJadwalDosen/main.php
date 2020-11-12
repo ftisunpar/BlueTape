5016c80b8da45e2adf5c3dc34c36d96eb3fb0388
stephenhadi <stephenhadi123@gmail.com>
2020-11-12 16:46:29 +0700

    fix column width

--- www/application/views/EntriJadwalDosen/main.php
+++ www/application/views/EntriJadwalDosen/main.php
@@ -72,7 +72,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                             <th></th>
                             <?php
                             for ($i = 0; $i < 5; $i++) {
-                                echo "<th> $namaHari[$i] </th>"; //Membuat Header Tabel yang berisi daftar hari
+                                echo "<th style='width:18%'> $namaHari[$i] </th>"; //Membuat Header Tabel yang berisi daftar hari
                             }
                             ?>
                         </tr>
