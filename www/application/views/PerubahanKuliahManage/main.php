ce96fb35b1e2b0ec202a21ac0e255e46a212613b
stephenhadi <stephenhadi123@gmail.com>
2021-01-20 22:08:19 +0700

    perbaikan href

--- www/application/views/PerubahanKuliahManage/main.php
+++ www/application/views/PerubahanKuliahManage/main.php
@@ -31,7 +31,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 <a class="nav-link" data-toggle="tab" href="#" id="byDay">Statistik Berdasarkan Hari</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" data-toggle="tab" href="byHour">Statistik Berdasarkan Jam</a>
+                                <a class="nav-link" data-toggle="tab" href="#">Statistik Berdasarkan Jam</a>
                             </li>
                         </ul>
                         <div class="tab-content">
