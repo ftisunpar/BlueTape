f149c3bc471293f41eb2c70de1fb5555cc1ea259
Pasal Alfadian Nugroho <pascalalfadian@live.com>
2022-03-08 14:44:54 +0700

    Revise e-mail recipients, show DNM link without NPM

--- www/application/config/modules.php
+++ www/application/config/modules.php
@@ -22,7 +22,7 @@ $config['modules'] = array(
 
 $config['roles'] = array(
     'root' => array('pascal@unpar.ac.id', 'shao.wei@unpar.ac.id'),
-    'tu.ftis' => array('shao.wei@unpar.ac.id', 'purnomo@unpar.ac.id', 'walip@unpar.ac.id'),
+    'tu.ftis' => array('kasubag_akd.ftis@unpar.ac.id', 'walip@unpar.ac.id'),
     'mahasiswa.ftis' => '(7[123]\\d{5})|(20[1-9][0-9]7[123][0-9]{4})|(61[678][0-9]{7})@student\\.unpar\\.ac\\.id',
     'staf.unpar' => '.+@unpar\\.ac\\.id',
     'dosen.informatika' => array ('cheni@unpar.ac.id', 'mariskha@unpar.ac.id', 'anung@unpar.ac.id', 'moertini@unpar.ac.id', 'natalia@unpar.ac.id', 'chandraw@unpar.ac.id', 'elisatih@unpar.ac.id', 'gkarya@unpar.ac.id', 'husnulhakim@unpar.ac.id', 'joanna@unpar.ac.id', 'lionov@unpar.ac.id', 'luciana@unpar.ac.id', 'pascal@unpar.ac.id', 'rosad5@unpar.ac.id', 'vania.natali@unpar.ac.id', 'kristopher.h@unpar.ac.id', 'raymond.chandra@unpar.ac.id', 'keenan.leman@unpar.ac.id', 'ignasiuserwin@unpar.ac.id'),
